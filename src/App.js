b0f63e726169f61508c8a30ed6e9020ea66beb16
Arash@m <alidadiarash71@gmail.com>
2023-06-04 20:23:06 +0430

    Add chatengine component and register in chatengine.io site

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import './App.css';
+import { ChatEngine } from 'react-chat-engine'
 const App = () => {
   return (
-    <div>App</div>
+   <ChatEngine
+   height="100vh"
+   projectID="ba2a7cfb-d7cb-4ef9-bde7-ce2dc7d4def1"
+   userName="Arasham"
+   userSecret="12341234"
+   />
   )
 }
 
