bc3c6891e195cee532553d6bf80bdc3b5156d130
Arash@m <alidadiarash71@gmail.com>
2023-06-05 01:57:17 +0430

    fix bug --login a user not another

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const App = () => {
    <ChatEngine
    height="100vh"
    projectID="ba2a7cfb-d7cb-4ef9-bde7-ce2dc7d4def1"
-   userName="Arasham"
-   userSecret="12341234"
+   userName={localStorage.getItem('username')}
+   userSecret={localStorage.getItem('password')}
    renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
    />
   )
